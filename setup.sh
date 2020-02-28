36356eec845423ea75a0788960f090766e43b346
patricksferraz <patrick.ferraz@outlook.com>
2020-02-28 19:37:50 +0000

    feat(docker, setup): adds mult softs install

--- setup.sh
+++ setup.sh
@@ -11,36 +11,47 @@ then
 fi
 
 helpMessage() {
-   echo "Usage: `basename $0` [-hv] <-i>"
+   echo "Usage: `basename $0` [-hv][-i software]"
    echo "Options:"
-   echo -e "\ti: install all stack"
+   echo -e "\ti: software to install"
    echo -e "\th: see this message"
    echo -e "\tv: get software version"
+   echo "Softwares:"
+   echo -e "\tbasic: only dependencies and oh-my-zsh"
+   echo -e "\tconda"
+   echo -e "\tdocker"
+   echo -e "\tall: install all softwares"
    exit
 }
 
-while getopts "hvi" OPT
+while getopts "hvi:" OPT
 do
     case $OPT in
         h) helpMessage ;;
         v)
             echo "`basename $0` version 0.1"
-            exit
-            ;;
-        i) DO_INSTALL=1 ;;
+            exit ;;
+        i)
+            DO_INSTALL=1
+            softs+=$OPTARG ;;
         ?) helpMessage ;;
     esac
 done
-# shift $((OPTIND-1))
-
-if [ -z $1 ]
-then
-    helpMessage
-fi
+shift $((OPTIND-1))
 
 if [ $DO_INSTALL ]
 then
     install/./up.sh
-    install/softwares/./dependencies.sh
-    install/softwares/./miniconda.sh
+    for soft in ${softs[@]}
+    do
+        case $soft in
+            basic) sh install/softwares/./dependencies.sh ;;
+            conda) install/softwares/./miniconda.sh ;;
+            docker) install/softwares/./docker.sh ;;
+            all)
+                install/softwares/./dependencies.sh
+                install/softwares/./miniconda.sh
+                install/softwares/./docker.sh ;;
+        esac
+    done
 fi
